89068f444ea5085219a9c40c74a1431853e688ba
sunimalherath <sunimaldv@gmail.com>
2021-02-27 18:11:55 +1100

    passing a map to template

---
+++ text-template/09_passing-map/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templt.gohtml"))
+}
+
+func main() {
+	months := map[string]int{
+		"Jan": 31,
+		"Feb": 28,
+		"Mar": 31,
+		"Apr": 30,
+		"May": 31,
+		"Jun": 30,
+		"Jul": 31,
+		"Aug": 31,
+		"Sep": 30,
+		"Oct": 31,
+		"Nov": 30,
+		"Dec": 31,
+	}
+
+	err := tpl.Execute(os.Stdout, months)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
